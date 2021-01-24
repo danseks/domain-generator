514d540c76c1b42a3eaed76759ae36a2eff59e65
danseks <danseksfzz@gmail.com>
2021-01-24 19:59:22 +0000

    First version of domain generator

--- src/main.js
+++ src/main.js
@@ -6,5 +6,38 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  generateDomainName();
 };
+
+function generateDomainName() {
+  let pronouns = ["the", "our"];
+  let adjs = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let extensions = [".com", ".es"];
+
+  let domain = "";
+  let domains = [];
+
+  for (let pronoun of pronouns) {
+    //console.log(pronouns);
+    for (let adj of adjs) {
+      //	console.log(adj[b]);
+      for (let noun of nouns) {
+        for (let extension of extensions) {
+          domain = domain.concat(pronoun, adj, noun, extension);
+          domains.push(domain);
+          domain = "";
+        }
+      }
+    }
+  }
+
+  let finalName = domains.forEach(function(name) {
+    let li = document.createElement("li");
+    // li.classList.add("dropdown-item");
+    var domainName = document.createTextNode(name);
+    li.appendChild(domainName);
+    document.getElementById("domainName").appendChild(li);
+  });
+  return finalName;
+}
